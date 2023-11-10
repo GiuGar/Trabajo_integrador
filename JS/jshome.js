84f4f179119dc2fe4e0aa89c476674e4d5bf53a9
CandelaSegura <mcsegura@udesa.edu.ar>
2023-11-10 10:12:23 -0300

    holaaa

--- JS/jshome.js
+++ JS/jshome.js
@@ -1,4 +1,5 @@
 let apikey = "1eea5cd13b45ef02982782ec6843507b"
+let imagen = document.querySelectorAll(".imagen")
 
 fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}`)
 .then(function(resp){
@@ -6,6 +7,10 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}`)
 })
 .then(function(data){
  console.log(data)
+ for(let i = 0; i <= 5; i++)
+    imagen.innerHTML = `
+        <img src=${data.results[i].poster_path}/>
+        `
 })
 .catch(function(error){
     console.log("Hay un error")
